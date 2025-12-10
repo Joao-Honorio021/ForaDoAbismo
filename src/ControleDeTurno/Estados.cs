82247c3fca0c6bad855437286b0e6d7a3c456d55
João Honorio <hjoaovictorlima@gmail.com>
2025-12-10 19:36:24 -0300

    feat: adicionar arquitetura inicial do projeto e organizar estrutura de pastas

---
+++ src/ControleDeTurno/Estados.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForaDoAbismo.src.controle_de_turno
+{
+    public class Estados
+    {
+
+    }
+}
---
+++ src/personagem/Personagem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForaDoAbismo.src.personagem
+{
+    internal class Personagem
+    {
+    }
+}
